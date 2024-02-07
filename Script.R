8d82bc29ec4d99b19a1eb320f5e4f8422ac5efa0
albertofrison <alberto.frison@gmail.com>
2024-02-07 20:43:57 +0100

    First Commit

---
+++ Script.R
@@ -0,0 +1,8 @@
+################################################################################
+# 
+# Maven LEGO Challenge
+# url: https://mavenanalytics.io/challenges/maven-lego-challenge/29
+# Made with ♥︎ by Alberto Frison
+# Created on February 2023
+#
+################################################################################
