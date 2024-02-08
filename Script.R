6cc4b726d2693440553da4293b1cd137ecf97957
albertofrison <alberto.frison@gmail.com>
2024-02-08 02:57:09 +0100

    Added environment preparatioand Data download and Unzip (working)

--- Script.R
+++ Script.R
@@ -1,8 +1,30 @@
 ################################################################################
 # 
-# Maven LEGO Challenge
+# Maven LEGO Challenge (in R)
 # url: https://mavenanalytics.io/challenges/maven-lego-challenge/29
 # Made with ♥︎ by Alberto Frison
 # Created on February 2023
 #
+################################################################################
+
+################################################################################
+# SECTION 01
+# ENVIRONMENT AND LIBRARIES PREPARATION
+
+rm(list=ls()) # clear environment
+library (tidyverse)
+################################################################################
+
+
+
+################################################################################
+# SECTION 02
+# DOWNLOAD AND UNZIP OF THE DATA
+
+temp <- tempfile()
+data_url <- "https://maven-datasets.s3.amazonaws.com/LEGO+Sets/LEGO+Sets.zip"
+file_name <- "lego_sets.csv"
+download.file(data_url, temp)
+lego_data <- read.csv(unz(temp, file_name)) 
+unlink(temp)
 ################################################################################
